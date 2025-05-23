6edc397735d37c2ebd86da069b4e44ee1ae5f7c3
dineshpatte <152471120+dineshpatte@users.noreply.github.com>
2025-05-23 21:59:10 +0530

    y

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,10 +10,7 @@ app.use(cors({
   credentials: true,
 }));
 
-app.options('*', cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
-}));
+
 
 
 app.use(express.json({ limit: "16kb" }));
